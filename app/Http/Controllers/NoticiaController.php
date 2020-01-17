95832c9b482e9e859240b500d4fc1fc5c751becf
José Trejo <joseatrejos>
2020-01-16 17:28:16 -0700

    Primera Práctica; faltan bugs por corregir

---
+++ app/Http/Controllers/NoticiaController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use app\Noticia;
+
+class NoticiaController extends Controller
+{
+    public function index() {
+        
+        // Esta variable $noticia adoptará las variables (en este caso tablas) como objetos de PHP del archivo Noticia.php
+        $noticias = Noticia::all();
+
+        /*$noticias = array();
+        $argumentos = array();
+
+        $noticias[] =
+            ['titulo' => 'Yaquis toman la carretera'];
+        $noticias[] =
+            ['titulo' => 'Pavimentan todas las calles!'];*/
+        
+        // La llave $noticias se vuelve una VARIABLE llamada noticias
+        $argumentos['noticias'] = $noticias;
+        
+        return view('noticias.index', $argumentos);
+    }
+}
---
+++ resources/views/noticias/index.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>
+            Noticias
+        </title>
+    </head>
+
+    <body>
+        @php
+            // var_dump($noticias) 
+        @endphp
+        <table>
+            <thead>
+                <th>
+                    Titulo
+                </th>
+                <th>
+                    Acciones
+                </th>
+            </thead>
+            <tbody>
+                @foreach($noticias as $noticia)
+                <tr>
+                    <td>
+                        {{$noticia->titulo]}}
+                    </td>
+
+                    <td>
+                        <button>
+                            Leer más...
+                        </button>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </body>
+</html>
